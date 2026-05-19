ec5ee75ecbfcbbe67637293a03cc688f6652e854
david.lipert <david.lipert@PC-U08-16>
2026-05-19 11:31:11 +0200

    vse

--- cookieclicker/MainWindow.xaml.cs
+++ cookieclicker/MainWindow.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 using System.Xml.Linq;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 
@@ -21,29 +22,72 @@ namespace cookieclicker
     public partial class MainWindow : Window
     {
         cookieshop shop;
+        private int cislo = 0;
+        private int zaklik = 10;
+        private int zasekundu = 1;
 
         public MainWindow()
         {
             InitializeComponent();
+
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += casTik;
+            timer.Start();
+
+        }
+
+        public void pridatZaKlik(int pocet)
+        {
+            zaklik += pocet;
+            TxtZaKlik.Text = "Počet za kliknuti: " + zaklik.ToString();
+        }
+
+        private void casTik(object? sender, EventArgs e)
+        {
+            cislo += zasekundu;
+            TxtCislo.Text = cislo.ToString();
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            int pocet = 0;
+
+            cislo+= zaklik;
+            TxtCislo.Text = cislo.ToString();
             
+
         }
 
+        public void ZaSekunduLabel
+        
         private void shopButon_Click(object sender, RoutedEventArgs e)
         {
             if (shop == null)
+
             {
+
                 shop = new cookieshop();
+
+                shop.Closed += (s, args) => shop = null;
+
             }
 
-            if (!shop.IsVisible)
+            if (shop.IsVisible)
+
             {
-                shop.Show();
+
+                shop.Activate();
+
             }
+
+            else
+
+            {
+
+                shop.Show();
+
+            }
+
         }
     }
 }
